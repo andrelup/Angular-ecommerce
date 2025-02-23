3573b27ce4f1e1a682397b8b5ce5ac358cda9954
André Pereira <andrelcop@gmail.com>
2025-02-23 20:34:06 +0100

    add shopcart icon on toolbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [ MatToolbarModule],
+  imports: [ MatToolbarModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+cartItemCount: number = 0;
 
 }
