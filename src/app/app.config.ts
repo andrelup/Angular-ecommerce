fb8e60a8120f0419f9797ae36448d2ed261e1da6
André Pereira <andrelcop@gmail.com>
2025-02-23 21:22:52 +0100

    add the last configs to use NgRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { productReducer } from './store/reducers/product.reducer';
+import { ProductEffects } from './store/effects/product.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideStore({ products: productReducer }),
+    provideEffects([ProductEffects])
   ]
 };
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,36 +1,53 @@
 
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
+import { selectProducts } from '../../store/selectors/product.selectors';
+
+import { clearCache, loadProducts } from '../../store/actions/product.actions';
 
 
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, FormsModule,CurrencyPipe, MatGridListModule, MatCardModule, MatIconModule],
+  imports: [CommonModule, FormsModule,CurrencyPipe, MatCardModule, MatIconModule],
   providers: [ProductService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  private store = inject(Store);
+  products$: Observable<Product[]> = this.store.select(selectProducts);
 
   searchText: string = '';
   products: Product[]= [];
 
   constructor(readonly productService: ProductService, readonly router: Router) {
   }
+  // ngOnInit(): void {
+  //   this.productService.getProducts().subscribe((data) => {
+  //     console.log(data);
+  //     this.products = data;
+  //   });
+  // }
+
+
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((data) => {
-      console.log(data);
-      this.products = data;
-    });
+    this.store.dispatch(loadProducts());
+  }
+
+  refreshProducts(): void {
+    this.store.dispatch(clearCache());
+    this.store.dispatch(loadProducts());
   }
   onSearch() {
     console.log(this.searchText);
@@ -45,4 +62,5 @@ export class ProductListComponent {
       console.log(data);
     });
   }
+
 }
