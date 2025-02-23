849e50d647d14a839a9326f73f24429c5f8e257a
André Pereira <andrelcop@gmail.com>
2025-02-23 22:20:34 +0100

    add paginator

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,13 +1,15 @@
 
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
@@ -19,7 +21,7 @@ import { clearCache, loadProducts } from '../../store/actions/product.actions';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, FormsModule,CurrencyPipe, MatCardModule, MatIconModule],
+  imports: [CommonModule, FormsModule,CurrencyPipe, MatCardModule, MatIconModule, MatPaginatorModule, NgIf, NgFor],
   providers: [ProductService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
@@ -30,16 +32,11 @@ export class ProductListComponent {
 
   searchText: string = '';
   products: Product[]= [];
+  currentPage = 1;
+  pageSize = 12;
 
   constructor(readonly productService: ProductService, readonly router: Router) {
   }
-  // ngOnInit(): void {
-  //   this.productService.getProducts().subscribe((data) => {
-  //     console.log(data);
-  //     this.products = data;
-  //   });
-  // }
-
 
   ngOnInit(): void {
     this.store.dispatch(loadProducts());
@@ -48,6 +45,10 @@ export class ProductListComponent {
   refreshProducts(): void {
     this.store.dispatch(clearCache());
     this.store.dispatch(loadProducts());
+  }
+  onPageChange(event: PageEvent) {
+    this.currentPage = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
   }
   onSearch() {
     console.log(this.searchText);
