7c88b9b52cd7fdd3abe3c268c6ac58b09ea208c1
André Pereira <andrelcop@gmail.com>
2025-02-23 12:59:45 +0100

    update main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
