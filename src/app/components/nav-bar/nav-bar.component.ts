f5f6e953a34c6636938be294c076145d1af1a354
André Pereira <andrelcop@gmail.com>
2025-02-23 15:18:32 +0100

    add logo and name app to toolbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [ MatToolbarModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
